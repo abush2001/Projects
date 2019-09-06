7c0c77ae8cc68ad216b0702e3b28655e58efc99f
abush2001 <54956940+abush2001@users.noreply.github.com>
2019-09-05 22:07:00 -0500

    Create Calculator.py
    A Simple Calculator made in Python 3.7, made using the Pycharm IDE.

---
+++ Calculator.py
@@ -0,0 +1,59 @@
+
+def add():
+    num1 = int(input("What is the first number you are adding?"))
+    num2 = int(input("What is the second number you are adding?"))
+    num3 = num1 + num2
+    print(num1, "+", num2, "=", num3)
+
+def subtract():
+    num1 = int(input("What is the first number you are subtracting?"))
+    num2 = int(input("What is the second number you are subtracting?"))
+    num3 = num1 - num2
+    print(num1, "-", num2, "=", num3)
+
+def multiply():
+    num1 = int(input("What is the first number you are multiplying?"))
+    num2 = int(input("What is the second number you are multiplying?"))
+    num3 = num1 * num2
+    print(num1, "*", num2, "=", num3)
+
+def divide():
+    num1 = int(input("What is the numerator?"))
+    num2 = int(input("What is the denominator?"))
+    num3 = num1 / num2
+    print(num1, "/", num2, "=", num3)
+
+def calculator():
+    mathQuestion = input("ok, do you want to add, subtract, multiply or divide?")
+    if mathQuestion == "add":
+        add()
+    if mathQuestion == "subtract":
+        subtract()
+    if mathQuestion == "divide":
+        divide()
+    if mathQuestion == "multiply":
+        multiply()
+
+def query():
+    question = input("Would you like to use the calculator? [yes/no]")
+    if question == "no":
+        print("ok, see ya later!")
+    if question == "yes":
+        calculator()
+
+
+query()
+
+def main():
+
+    cont = input("Are you finished?")
+    if cont == "yes":
+        print("See ya")
+        quit()
+    if cont == "no":
+        while cont == "no":
+            calculator()
+            main()
+
+
+main()
